e36ac66e6bb5b9ce9b68caf7dc7953d1e71dea7d
Thomas Cort <linuxgeek@gmail.com>
2021-04-08 06:27:26 -0400

    Router: wire in controllers

--- example/demo.js
+++ example/demo.js
@@ -6,16 +6,18 @@ const {
     Server,
     Router,
 } = require('..');
-const ToDoController = require('./lib/controllers/ToDoController');
+const { ToDoController } = require('./lib/controllers');
+const { ToDoRoutes } = require('./lib/routes');
 const path = require('path');
 
-const routes = require('./lib/routes/todo');
 const storage = new JSONStorage({ basedir: path.join(path.sep, 'tmp'), pk: 'id' });
 const logger = new ConsoleLogger();
-const toDoController = new ToDoController(storage, logger);
+const controllers = {
+    todo: new ToDoController(storage, logger),
+};
 
-const router = new Router({ logger });
-router.register(routes);
+const router = new Router({ controllers, logger });
+router.register(ToDoRoutes);
 
 new Server({ router }).listen(3000, () => logger.inProdEnv('listening. Visit http://localhost:3000/hello/Alice'));
 
---
+++ example/lib/routes/index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const ToDoRoutes = require('./ToDoRoutes');
+
+module.exports = {
+    ToDoRoutes,
+};
