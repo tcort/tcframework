def71455dd5ac39ae731caceb1e3963bb0f91c27
Thomas Cort <linuxgeek@gmail.com>
2021-04-09 07:03:51 -0400

    demo: implement DELETE /todos/:id

--- example/demo.js
+++ example/demo.js
@@ -33,24 +33,31 @@ router.get('/', async (req, res) => {
     await res.renderFile('index.tct');
 });
 
-// GET /todos/:id
+// Create - POST /todos/:id
+router.post('/todos', async (req, res) => {
+    const id = await req.controllers.todo.create(req.body);
+
+    res.setHeader('Location', `/todos/${id}`);
+    res.json(await req.controllers.todo.read(id));
+});
+
+// Read - GET /todos/:id
 router.get(/^\/todos\/(?<id>[A-Za-z0-9-]+)$/, async (req, res) => {
     res.json(await req.controllers.todo.read(req.params.id));
 });
 
-// POST /todos/:id
-router.post('/todos', async (req, res) => {
-    const id = await req.controllers.todo.create(req.body);
-
-    res.setHeader('Location', `/todos/${id}.json`);
-    res.json(await req.controllers.todo.read(id));
-});
-
-// PUT /todos/:id
+// Update (or create) - PUT /todos/:id
 router.put(/^\/todos\/(?<id>[A-Za-z0-9-]+)$/, async (req, res) => {
     await req.controllers.todo.upsert(req.params.id, req.body);
     res.json(await req.controllers.todo.read(req.params.id));
 });
+
+// Delete - DELETE /todos/:id
+router.delete(/^\/todos\/(?<id>[A-Za-z0-9-]+)$/, async (req, res) => {
+    await req.controllers.todo.delete(req.params.id);
+    res.json({ status: 'ok' });
+});
+
 
 new Server({ router }).listen(3000, () => logger.inProdEnv('Listening...'));
 process.on('exit', () => logger.inProdEnv('Exiting...'));
