d992d3234e5af425899901ce4d0d4d4f7e38005e
Thomas Cort <linuxgeek@gmail.com>
2021-04-09 05:21:23 -0400

    JSONStorage: fix basedir configuration

--- example/demo.js
+++ example/demo.js
@@ -9,23 +9,34 @@ const {
     Router,
 } = require('..');
 
-// import out controllers
 const { ToDoController } = require('./lib/controllers');
 
-// create a router
+
+// setup logging
 const logger = new ConsoleLogger();
+
+// setup storage
+const storage = new JSONStorage({ basedir: path.join(__dirname, 'storage') });
+
+// controllers
+const todo = new ToDoController(storage, logger);
+
+// setup router
 const router = new Router({
-    controllers: {
-        todo: new ToDoController(new JSONStorage(), logger),
-    },
+    controllers: { todo },
     viewsdir: path.join(__dirname, 'views'),
-    logger
+    logger,
 });
 
 // routes
 router.get('/', async (req, res) => {
     await res.renderFile('index.tct');
 });
+
+router.get(/^\/todos\/(?<id>[A-Za-z0-9-]+)$/, async (req, res) => {
+    res.json(await req.controllers.todo.read(req.params.id));
+});
+
 
 new Server({ router }).listen(3000, () => logger.inProdEnv('Listening...'));
 process.on('exit', () => logger.inProdEnv('Exiting...'));
