a4598ac9e2b29db99b3a9ef1b15b65e97c530faa
Thomas Cort <linuxgeek@gmail.com>
2021-04-11 06:37:01 -0400

    tcframework: import JSONPointer from the prototype

--- tcframework.js
+++ tcframework.js
@@ -773,6 +773,7 @@ class Validator {
     /**
      * Builds a boolean validator.
      *
+     * @static
      * @returns {BooleanValidator}
      */
     static boolean() {
@@ -782,6 +783,7 @@ class Validator {
     /**
      * Builds a string validator.
      *
+     * @static
      * @returns {StringValidator}
      */
     static string() {
@@ -791,6 +793,7 @@ class Validator {
     /**
      * Builds an object validator.
      *
+     * @static
      * @returns {ObjectValidator}
      */
     static object(obj = {}) {
@@ -1239,5 +1242,99 @@ class Controller {
 }
 
 module.exports.Controller = Controller;
+
+/**
+ * JSONPointer implements RFC6901 JavaScript Object Notation (JSON) Pointer
+ *
+ * @version 1.0.0
+ */
+class JSONPointer {
+
+    /**
+     * set a value in obj in the location pointed to by ptr.
+     *
+     * @static
+     * @param {object} obj - object to set the value in.
+     * @param {string} ptr - JSONPointer.
+     * @param {any} value - value to set.
+     */
+    static set(obj, ptr, value) {
+
+        if (typeof obj !== 'object' || obj == null) {
+            throw new JSONPointerError(obj, ptr, 'obj must be a non-null object');
+        }
+
+        if (typeof ptr !== 'string') {
+            throw new JSONPointerError(obj, ptr, 'ptr must be a string');
+        }
+
+        if (ptr === '') {
+            return value;
+        }
+
+        if (ptr[0] !== '/') {
+            throw new JSONPointerError(obj, ptr, 'non-empty ptr must begin with a "/"');
+        }
+
+        const components = ptr
+                                .split('/')                                         // break up /foo/bar into ['foo','bar']
+                                .map((component) => component.replace(/~1/g, '/'))  // '~1' to '/'
+                                .map((component) => component.replace(/~0/g, '~'))  // '~0' to '~'
+                                .slice(1);                                          // chop off array head (which will be '')
+
+        let cur = obj;
+        components.slice(0, -1).forEach((component) => {
+            if (!['object','array'].includes(typeof cur[component]) || cur[component] === null) {
+                cur[component] = {};
+            }
+            cur = cur[component];
+        });
+        cur[components[components.length - 1]] = value;
+
+        return obj;
+    }
+
+    /**
+     * get a value from obj pointed to by ptr.
+     *
+     * @static
+     * @param {object} obj - object to get the value from.
+     * @param {string} ptr - JSONPointer.
+     */
+    static get(obj, ptr) {
+
+        if (typeof obj !== 'object' || obj == null) {
+            throw new JSONPointerError(obj, ptr, 'obj must be a non-null object');
+        }
+
+        if (typeof ptr !== 'string') {
+            throw new JSONPointerError(obj, ptr, 'ptr must be a string');
+        }
+
+        if (ptr === '') {
+            return obj;
+        }
+
+        if (ptr[0] !== '/') {
+            throw new JSONPointerError(obj, ptr, 'non-empty ptr must begin with a "/"');
+        }
+
+        const components = ptr
+                                .split('/')                                         // break up /foo/bar into ['foo','bar']
+                                .map((component) => component.replace(/~1/g, '/'))  // '~1' to '/'
+                                .map((component) => component.replace(/~0/g, '~'))  // '~0' to '~'
+                                .slice(1);                                          // chop off array head (which will be '')
+
+        return components.reduce((result, component) => {
+            if (typeof result !== 'object' || result == null) {
+                return undefined; // path not found
+            }
+            return result[component];
+        }, obj);
+    }
+
+}
+
+module.exports.JSONPointer = JSONPointer;
 
 
