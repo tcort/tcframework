ad695cfb411eb2b52f0522082bafd46c74ef0e12
Thomas Cort <linuxgeek@gmail.com>
2021-04-11 08:00:02 -0400

    tcframework: import bodyParser from the prototype

--- tcframework.js
+++ tcframework.js
@@ -1908,3 +1908,33 @@ class Route {
 }
 
 module.exports.Route = Route;
+
+/**
+ * Collects data from a stream into one UTF-8 string.
+ * When contentType is application/json, it is parsed into an object
+ *
+ * @param {string} contentType - type of the content (e.g. 'application/json').
+ * @param {Stream} stream - a stream object.
+ */
+async function bodyParser(contentType, stream) {
+
+    return await new Promise((resolve, reject) => {
+        let body = '';
+
+        stream.setEncoding('utf-8');
+        stream.on('data', (chunk) => body += chunk.toString());
+        stream.on('err', (err) => reject(err));
+        stream.on('end', () => {
+            switch (contentType) {
+                case 'application/json':
+                    body = JSON.parse(body);
+                    break;
+                default:
+                    break;
+            }
+            resolve(body);
+        });
+    });
+}
+
+module.exports.bodyParser = bodyParser;
